bd05fb610b5afb324d9b4e2d7bbb07c0f6510b84
ahmadgabariin <ahmadhamze.kawasaki@gmail.com>
2022-06-03 13:44:05 +0300

    heroku and mongodb atals added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require(`express`)
 const path = require(`path`)
 const app = express()
-const port = 3000
+const port = 8080
 const api = require(`./server/routers/api`)
 const mongoose = require(`mongoose`)
-
+const connectionURL = require(`./config`).connectionURL
 
 mongoose.connect(process.env.MONGODB_URL || `mongodb://localhost/expensesDB` , { useNewUrlParser: true } )
 
